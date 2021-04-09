366469ea79ba80ec651db660f277643a8735b3bc
Alex Steele <steeleal123@gmail.com>
2021-04-09 13:23:37 -0700

    connect: support diskdb

--- pydb/connect.py
+++ pydb/connect.py
@@ -1,6 +1,7 @@
 import urllib.parse
 
 from .core import Database
+from .disk import DiskDatabase
 from .mem import MemDatabase
 
 
@@ -8,5 +9,7 @@ def connect(url, *flags, **options) -> Database:
     url = urllib.parse.urlparse(url)
     if url.scheme == "mem":
         return MemDatabase(url.path)
+    elif url.scheme in ("disk", ""):
+        return DiskDatabase.open(url.path)
     else:
         raise ValueError("unrecognized scheme: {}".format(url.scheme))
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 from . import context
 import unittest
 import pydb
+import tempfile
 from pydb.query import CreateTable, From, Insert, Select
 from pydb.table import Column, ColumnAttr, DataType, Schema
 
@@ -18,3 +19,10 @@ class PydbInterfaceTestCase(unittest.TestCase):
             db.exec(Insert("students", ("id", "name"), (0, "ack")))
             result = db.exec(Select(("id", "name"), From("students")))
             assert list(result) == [(0, "ack")]
+
+    def test_connect_diskdb(self):
+        with tempfile.TemporaryDirectory() as folder:
+            with pydb.connect(folder) as db:
+                assert db
+            with pydb.connect("disk:" + folder) as db:
+                assert db
